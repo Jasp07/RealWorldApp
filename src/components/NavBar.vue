c9f34fee5b931e5ed0649017d84565a89f97bc57
Jaspreet Singh | Nagarro <jaspreet.singh02@nagarro.com>
2018-12-10 00:22:45 +0530

    Unit and e2e tests with coverage report

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,10 @@
 <template>
     <nav class="navbar navbar-light">
       <div class="container">
-        <a class="navbar-brand" href="index.html">conduit</a>
+        <router-link class="navbar-brand" to="/">
+                conduit
+        </router-link>
+        <!-- <a class="navbar-brand" href="index.html">conduit</a> -->
         <ul class="nav navbar-nav pull-xs-right">
           <li class="nav-item">
             <!-- Add "active" class when you're on that page" -->
@@ -9,27 +12,41 @@
                 Home
             </router-link>
           </li>
-          <li class="nav-item">
+          <li v-if="username" class="nav-item">
               <router-link class="nav-link" to="/editor">
                  <i class="ion-compose"></i>&nbsp;New Post
               </router-link>
           </li>
-          <li class="nav-item">
+          <li v-if="username" class="nav-item">
               <router-link class="nav-link" to="/settings">
                  <i class="ion-gear-a"></i>&nbsp;Settings
               </router-link>
           </li>
-          <li class="nav-item">
+          <li v-if="username == null" class="nav-item">
               <router-link class="nav-link" to="/login">
                 Sign in
               </router-link>
           </li>
-          <li class="nav-item">
+          <li v-if="username == null" class="nav-item">
               <router-link class="nav-link" to="/register">
                 Sign up
+              </router-link>
+          </li>
+          <li v-if="username" class="nav-item">
+              <router-link class="nav-link" :to="`/@${username}`">
+                {{ username }}
               </router-link>
           </li>
         </ul>
       </div>
     </nav>
 </template>
+<script>
+export default {
+  computed: {
+    username() {
+      return this.$store.getters["users/username"];
+    }
+  }
+};
+</script>
